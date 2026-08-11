fdcc1479d0101df926f341cc36b398fce04cc8b9
sumantpulparthi <sumantpulparthi@gmail.com>
2026-08-11 16:09:12 +0530

    Add dataVariables.spec.ts for validating data variable scope

---
+++ HRM_Test/dataVariables.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+function addValue(x: number, y: number): String {
+  let total = x + y;
+  return `Total Value of ${x} & ${y} : ${total}`;
+}
+
+test("Validate Data Variables Scope", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  let x = 20;
+  let y = 60;
+
+  console.log(addValue(x, y));
+});
