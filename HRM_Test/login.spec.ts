c280fa9c7f2deb303fc2c8c9ad8fd007a97e8ec4
sumantpulparthi <sumantpulparthi@gmail.com>
2026-08-11 17:22:42 +0530

    Add a blank line for better readability in login.spec.ts

--- HRM_Test/login.spec.ts
+++ HRM_Test/login.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 
 let url: string =
   "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
 test("Login to Orange HRM", async ({ page }) => {
   await page.goto(url);
   console.log(`URL : ${url}`);
